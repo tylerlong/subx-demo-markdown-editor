a6efa811eaceb1eabf107f7563f6df69b3c1f6e3
Tyler Long <tyler4long@gmail.com>
2018-10-02 07:58:25 +0800

    Use react-subx

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { debounceTime } from 'rxjs/operators'
+import { Component } from 'react-subx'
 
-class Editor extends React.Component {
-  componentWillMount () {
+class Editor extends Component {
+  constructor (props) {
+    super(props)
     this.article = this.props.article
-    this.propsSubscription = this.article.$.subscribe(() => this.forceUpdate())
-    this.htmlSubscription = this.article.$.pipe(debounceTime(1000)).subscribe(event => {
-      this.html = this.article.html
-      this.forceUpdate()
-    })
+    this.state = {
+      html: ''
+    }
+    this.htmlSubscription = this.article.$.pipe(debounceTime(1000)).subscribe(event => this.setState({ html: this.article.html }))
   }
   componentWillUnmount () {
-    this.propsSubscription.unsubscribe()
     this.htmlSubscription.unsubscribe()
   }
   render () {
@@ -20,7 +20,7 @@ class Editor extends React.Component {
       <div>
         <textarea placeholder='Please enter some markdown...' id='markdown-textarea'
           value={this.article.text} onChange={e => { this.article.text = e.target.value }} />
-        <div className='markdown-body' dangerouslySetInnerHTML={{ __html: this.html }} />
+        <div className='markdown-body' dangerouslySetInnerHTML={{ __html: this.state.html }} />
       </div>
     )
   }
